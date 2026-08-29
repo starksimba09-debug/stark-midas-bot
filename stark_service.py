5e3b4949a7530d488939dd9548b2dc2b31391b9f
starksimba09-debug <starksimba09@gmail.com>
2026-08-30 01:03:08 +0300

    Update message and request headers in stark_service.py

--- stark_service.py
+++ stark_service.py
@@ -6,10 +6,18 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(func=lambda message: True)
 def handle_all_messages(message):
-    bot.reply_to(message, "🚀 جاري جلب البيانات...")
+    bot.reply_to(message, "🚀 جاري جلب البيانات بجلسة الحساب النشطة...")
     
     try:
-        # استخدام curl_cffi للتخفي وتخطى حماية Midasbuy بمتصفح وهمي (Chrome 120)
+        # تجميع الكوكيز الحقيقية من متصفحك
+        cookies = (
+            "session_token=9ca8360c3d76626452367a14ea5060b786e9cc3a2979e6c04f746c9ae03627b3; "
+            "midasbuyDeviceId=0092166902720319581788028726879; "
+            "UUID=07666804840699624178804062466468871; "
+            "forterToken=af9908bdb7444b4694c34638e4973aa6_1788040690745__UDF43-m4_27ck_; "
+            "country=eg; select_country=eg; shopcode=midasbuy"
+        )
+        
         response = requests.post(
             'https://pagedooapi.midasbuy.com/api/CallMpgo/osmidas/dd_help_model/HelpInfoListByUserId',
             impersonate="chrome120",
@@ -17,7 +25,8 @@ def handle_all_messages(message):
                 'content-type': 'application/json',
                 'accept': 'application/json, text/plain, */*',
                 'origin': 'https://www.midasbuy.com',
-                'referer': 'https://www.midasbuy.com/'
+                'referer': 'https://www.midasbuy.com/eg/buy/pubgm?adtag=event.couponhelper',
+                'cookie': cookies
             },
             json={
                 "mp_activity_id": "Activity_1784618952_EQXYLI",
